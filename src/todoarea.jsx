270f20f4e72cd0b12bceea7e22fa7287a6b66e06
Lichtリヒト <sharmarahul1729@gmail.com>
2026-01-23 10:30:16 +0530

    fix:Added Fix to print the UserEffect Task details

--- src/todoarea.jsx
+++ src/todoarea.jsx
@@ -17,7 +17,7 @@ export function TodoArea() {
     const getData = async () =>  {
       try{
         console.log("BASE_URL",BASE_URL);
-      const result = await axios.get(`${BASE_URL}/data`);
+      const result = await axios.get(`https://proficiently-bibliophagous-wilma.ngrok-free.dev/data`);
        console.log("DATTTTTTTTTTTTTT",result)
       console.log("DATTTTTTTTTTTTTT",result.data.data)
       setTasks(result.data.data)
